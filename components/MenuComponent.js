76ff2ffa6cfe0f1b03279c70ed90a0eb71eef3f0
Azher Saeed Ch <chazher1997@gmail.com>
2020-05-26 19:40:18 +0500

    dishes render by clicking

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -7,7 +7,7 @@ function MenuComponent (props) {
     const renderMenuItem = ({index , item}) => {
         return(
             <ListItem key={index} title={item.name} subtitle={item.description} hideChevron={true} 
-            leftAvatar = {{source : require('./images/buffet.png')}} />
+            leftAvatar = {{source : require('./images/buffet.png')}} onPress={() => props.onPress(item.id)} />
         )
     }
     return (
